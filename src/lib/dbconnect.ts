d9dfe7c64f38f6dcd61bee1c3b4235d1dcac169a
DhammaMendhe <send2dhammadipmendhe@gmail.com>
2025-09-16 12:53:49 +0530

    for user model

---
+++ src/lib/dbconnect.ts
@@ -0,0 +1,20 @@
+import mongoose  from "mongoose";
+
+
+
+type connetionObject = {
+    isConnected?:number
+}
+
+const connection :connetionObject = {
+
+
+
+}
+
+
+async function dbConnect(): Promise<void> {
+if(connection:isco)
+
+    
+}
